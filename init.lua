240f694bd8d546062e0ccdb887fe5b39b0a5bbae
Farhan Ahmad Nurzi <58872254+Farhan-slurrp@users.noreply.github.com>
2024-08-28 21:12:05 +0800

    init commit

---
+++ init.lua
@@ -0,0 +1,22 @@
+require("core.mappings")
+require("core.colorscheme")
+require("core.options")
+require("core.plugins")
+
+-- Set compatibility to Vim only
+vim.cmd('set nocompatible')
+
+-- Auto text wrapping
+vim.wo.wrap = true
+
+-- Encoding
+vim.o.encoding = 'utf-8'
+
+-- Show line numbers
+vim.wo.number = true
+
+-- Status bar
+vim.o.laststatus = 2
+
+-- Indent width
+vim.o.shiftwidth = 2
